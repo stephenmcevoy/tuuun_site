e4bba28c91918f25b365cc522be0cfca3cc9eaba
Stephen McEvoy <stephenwmcevoy@gmail.com>
2018-02-12 15:44:37 +0100

    delete dropdowns, simplify

--- javascript/tuuun.js
+++ javascript/tuuun.js
@@ -1,17 +1,5 @@
 $(document).ready(function(e) {
 
-  console.log("loaded");
-
-$(".dropdown-head").click(function(x) {
-  x.stopPropagation();
-  $(".dropdown-head").not(this).find("div").removeClass("visible");
-  $(this).find(".dropdown-content").toggleClass("visible");
-
-});
-
-$(document).click(function(){
-  $(".dropdown-content").removeClass("visible");
-});
 
 
 });
