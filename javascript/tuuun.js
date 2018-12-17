146c5058deea160658b8d566e2a7e5b97454aefa
Stephen McEvoy <stephenwmcevoy@gmail.com>
2018-12-17 15:19:58 +0100

    new site

--- javascript/tuuun.js
+++ javascript/tuuun.js
@@ -1,5 +1,14 @@
 $(document).ready(function(e) {
 
+  $(".expand").click(function() {
+  // alert("clicked!");
+  $(this).find("img").slideToggle();
+});
 
+  $(".title").click(function() {
+  // alert("clicked!");
+  $(this).find(".description").slideToggle();
+  $(this).find("img").slideToggle();
+});
 
 });
